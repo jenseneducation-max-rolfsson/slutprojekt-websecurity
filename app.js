9bea8f63b3256209ee1397419055c7f8b63d3cd3
jenseneducation-max-rolfsson <54712545+jenseneducation-max-rolfsson@users.noreply.github.com>
2020-02-03 13:54:10 +0100

    inloggning
    problem med error message om requesten har timat ut (rad 31)

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const express = require('express')
 const app = express()
+const jwt = require('jsonwebtoken')
+const jwtVerifier = require('express-jwt')
+
+const secret = 'secret';
 
 app.use(express.static('public'))
+app.use(express.json())
 
+const user = { email: 'kurwa@kurwamail.com' , password: '1234567' }
+
+function createToken(){
+    const expirationDate = Math.floor(Date.now() /1000) + 30;
+    const token = jwt.sign({userID: user.email, exp: expirationDate}, secret);
+    return token
+}
+
+app.post('/auth', (req,res) => {
+if (req.body.email == user.email && req.body.password == user.password){
+    res.send(createToken())
+}else {
+    res.sendStatus(400)
+}
+})
+
+app.get('/verify',jwtVerifier({secret:secret}), (req,res) => {
+    res.send("inloggad");
+})
+
+app.use((err, req, res, next) => {
+    if( err.name === 'UnathorizedError'){
+        res.status(500).send(err.message);
+    }
+});
 
 app.listen(8080, () => console.log("Server started"))
